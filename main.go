88f63f216021ff3925c5d200e82158350e1f2f55
Thomas Queste <tom@tomsquest.com>
2021-11-14 16:52:55 +0100

    feat: count down

--- main.go
+++ main.go
@@ -1,31 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
 	"image/color"
+	"time"
 )
 
 func main() {
-	theApp := app.NewWithID("com.tomquest.fynodoro")
-	theApp.Settings().SetTheme(&myTheme{})
+	myApp := app.NewWithID("com.tomquest.fynodoro")
+	myApp.Settings().SetTheme(&myTheme{})
 
-	win := theApp.NewWindow("Fynodoro")
-	win.SetIcon(resourceIconPng)
-	win.Resize(fyne.NewSize(400, 200))
-	win.CenterOnScreen()
+	myWin := myApp.NewWindow("Fynodoro")
+	myWin.SetIcon(resourceIconPng)
+	myWin.CenterOnScreen()
 
+	myCanvas := myWin.Canvas()
+
+	timerDuration := 25 * 60 * time.Second
+	go updateTimer(myCanvas, timerDuration)
+
+	myWin.ShowAndRun()
+}
+
+func updateTimer(myCanvas fyne.Canvas, timerDuration time.Duration) {
+	displayTimer(myCanvas, timerDuration)
+
+	timer := time.NewTimer(timerDuration)
+	ticker := time.NewTicker(time.Second)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				timerDuration -= time.Second
+				displayTimer(myCanvas, timerDuration)
+			case <-timer.C:
+				return
+			}
+		}
+	}()
+}
+
+func formatDuration(d time.Duration) string {
+	d = d.Round(time.Second)
+	m := int(d.Minutes())
+	s := int(d.Seconds()) % 60
+	return fmt.Sprintf("%02d:%02d", m, s)
+}
+
+func displayTimer(myCanvas fyne.Canvas, timeLeft time.Duration) {
 	red := color.NRGBA{R: 180, G: 0, B: 0, A: 255}
-	text := canvas.NewText("25:00", red)
-	text.TextStyle.Monospace = true
+	text := canvas.NewText(formatDuration(timeLeft), red)
 	text.TextStyle.Bold = true
 	text.TextSize = 100
-
-	content := container.New(layout.NewMaxLayout(), text)
-	win.SetContent(content)
-
-	win.ShowAndRun()
+	myCanvas.SetContent(text)
 }
