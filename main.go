3ba4fcfc7b0372d740a4a4808a8ebe26896f21a2
Thomas Queste <tom@tomsquest.com>
2021-11-18 13:58:54 +0100

    feat: add play/pause, stop buttons

--- main.go
+++ main.go
@@ -1,12 +1,79 @@
 package main
 
 import (
-	"fyne.io/fyne/v2"
+	"fmt"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
-	"image/color"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 	"time"
 )
+
+type pomodoro struct {
+	period    time.Duration
+	remaining time.Duration
+	running   bool
+	ticker    *time.Ticker
+	timer     *time.Timer
+	onTick    func(time.Duration)
+	onEnd     func()
+}
+
+func NewPomodoro(period time.Duration) *pomodoro {
+	p := new(pomodoro)
+	p.period = period
+	p.remaining = period
+	p.running = false
+	return p
+}
+func (p *pomodoro) Start() {
+	fmt.Println("Start", p.remaining)
+	p.ticker = time.NewTicker(time.Second)
+	p.timer = time.NewTimer(p.remaining)
+	p.running = true
+
+	go func() {
+		for {
+			select {
+			case <-p.ticker.C:
+				p.remaining -= time.Second
+
+				if p.onTick != nil {
+					p.onTick(p.remaining)
+				}
+			case <-p.timer.C:
+				p.stop()
+
+				if p.onEnd != nil {
+					p.onEnd()
+				}
+
+				return
+			}
+		}
+	}()
+}
+func (p *pomodoro) Pause() {
+	fmt.Println("Pause", p.remaining)
+	p.stop()
+}
+func (p *pomodoro) Stop() {
+	fmt.Println("Stop")
+	p.stop()
+	p.remaining = p.period
+}
+func (p *pomodoro) stop() {
+	p.running = false
+
+	if p.ticker != nil {
+		p.ticker.Stop()
+	}
+	if p.timer != nil {
+		p.timer.Stop()
+	}
+}
 
 func main() {
 	myApp := app.NewWithID("com.tomquest.fynodoro")
@@ -16,40 +83,54 @@ func main() {
 	myWin.SetIcon(resourceIconPng)
 	myWin.CenterOnScreen()
 
-	myCanvas := myWin.Canvas()
-
 	timerDuration := 25 * 60 * time.Second
-	go updateTimer(myCanvas, timerDuration)
+	pomodoro := NewPomodoro(timerDuration)
 
-	myWin.ShowAndRun()
-}
+	timer := canvas.NewText(formatDuration(pomodoro.period), nil)
+	timer.TextSize = 72
 
-func updateTimer(myCanvas fyne.Canvas, timerDuration time.Duration) {
-	displayTimer(myCanvas, timerDuration)
+	startButton := widget.NewButtonWithIcon("Start", theme.MediaPlayIcon(), nil)
+	startButton.OnTapped = func() {
+		if pomodoro.running {
+			fmt.Println("Pause")
+			startButton.Icon = theme.MediaPlayIcon()
+			startButton.Text = "Start"
+			startButton.Refresh()
 
-	timer := time.NewTimer(timerDuration)
-	ticker := time.NewTicker(time.Second)
+			pomodoro.Pause()
+		} else {
+			fmt.Println("Start")
+			startButton.Icon = theme.MediaPauseIcon()
+			startButton.Text = "Pause"
+			startButton.Refresh()
 
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				timerDuration -= time.Second
-				displayTimer(myCanvas, timerDuration)
-			case <-timer.C:
-				// Force the time to be 0 given the timer can end just a bit before the tick,
-				// and the final time be 1 instead of 0.
-				displayTimer(myCanvas, 0)
-				return
-			}
+			pomodoro.Start()
 		}
-	}()
-}
+	}
+	stopButton := widget.NewButtonWithIcon("Stop", theme.MediaStopIcon(), func() {
+		pomodoro.Stop()
+		timer.Text = formatDuration(pomodoro.period)
+		timer.Refresh()
 
-func displayTimer(myCanvas fyne.Canvas, timeLeft time.Duration) {
-	red := color.NRGBA{R: 180, G: 0, B: 0, A: 255}
-	text := canvas.NewText(formatDuration(timeLeft), red)
-	text.TextStyle.Bold = true
-	text.TextSize = 100
-	myCanvas.SetContent(text)
+		startButton.Icon = theme.MediaPlayIcon()
+		startButton.Text = "Start"
+		startButton.Refresh()
+	})
+	buttons := container.NewHBox(layout.NewSpacer(), startButton, stopButton, layout.NewSpacer())
+
+	pomodoro.onTick = func(remainingTime time.Duration) {
+		fmt.Println("onTick", remainingTime)
+		timer.Text = formatDuration(remainingTime)
+		timer.Refresh()
+	}
+	pomodoro.onEnd = func() {
+		fmt.Println("onEnd")
+
+		startButton.Icon = theme.MediaPlayIcon()
+		startButton.Text = "Start"
+		startButton.Refresh()
+	}
+
+	myWin.SetContent(container.NewVBox(timer, buttons))
+	myWin.ShowAndRun()
 }
