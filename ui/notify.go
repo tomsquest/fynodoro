d9c9f054bb74997ed3fe6bb135df1f12eb73e6a9
Thomas Queste <tom@tomsquest.com>
2021-12-27 07:19:07 +0100

    feat: display Fynodoro icon in notifications

--- ui/notify.go
+++ ui/notify.go
@@ -9,5 +9,5 @@ import (
 func notifyPomodoroDone(kind pomodoro.Kind) {
 	title := fmt.Sprintf("%s done", kind)
 	message := fmt.Sprintf("You just finished a %s pomodoro.", kind)
-	_ = beeep.Notify(title, message, "")
+	_ = beeep.Notify(title, message, "/usr/share/pixmaps/fynodoro.png")
 }
