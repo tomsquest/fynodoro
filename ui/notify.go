16a3f651a21d99badf75a0baa45a5a41bbd92d23
Andy Williams <andy@andy.xyz>
2021-12-28 11:47:39 -0800

    Remove beep dep for notifications

--- ui/notify.go
+++ ui/notify.go
@@ -2,12 +2,14 @@ package ui
 
 import (
 	"fmt"
-	"github.com/gen2brain/beeep"
+
 	"github.com/tomsquest/fynodoro/pomodoro"
+
+	"fyne.io/fyne/v2"
 )
 
 func notifyPomodoroDone(kind pomodoro.Kind) {
 	title := fmt.Sprintf("%s done", kind)
 	message := fmt.Sprintf("You just finished a %s pomodoro.", kind)
-	_ = beeep.Notify(title, message, "/usr/share/pixmaps/fynodoro.png")
+	fyne.CurrentApp().SendNotification(fyne.NewNotification(title, message))
 }
