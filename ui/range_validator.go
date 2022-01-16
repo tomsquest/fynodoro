eb36c7fd9ad7881148b4e89b135069d296a65fd7
Thomas Queste <tom@tomsquest.com>
2022-01-16 07:22:04 +0100

    test: range_validator.go

--- ui/range_validator.go
+++ ui/range_validator.go
@@ -11,13 +11,13 @@ func NewRangeValidator(min int64, max int64) fyne.StringValidator {
 	return func(text string) error {
 		v, err := strconv.ParseInt(text, 10, 64)
 		if err != nil {
-			return errors.New("Not a valid number")
+			return errors.New("not a valid number")
 		}
 		if v < min {
-			return errors.New(fmt.Sprintf("Must be greater that %d", min))
+			return fmt.Errorf("must be greater than %d", min)
 		}
 		if v > max {
-			return errors.New(fmt.Sprintf("Must be lesser than %d", max))
+			return fmt.Errorf("must be lesser than %d", max)
 		}
 
 		return nil // Nothing to validate with, same as having no validator.
