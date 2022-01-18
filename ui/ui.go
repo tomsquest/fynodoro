1b3dd9ba37984628216bf7efd4afe7080b786033
Thomas Queste <tom@tomsquest.com>
2022-01-18 07:03:44 +0100

    refactor: rename startButton to playButton, because it can start/pause

--- ui/ui.go
+++ ui/ui.go
@@ -34,21 +34,21 @@ func MakeClassicLayout(app fyne.App, myPomodoro *pomodoro.Pomodoro) fyne.CanvasO
 	timerButton := widget.NewButton("", nil)
 	timerPanel := container.NewHBox(layout.NewSpacer(), container.NewMax(timer, timerButton), layout.NewSpacer())
 
-	startButton := widget.NewButtonWithIcon("", theme.MediaPlayIcon(), nil)
+	playButton := widget.NewButtonWithIcon("", theme.MediaPlayIcon(), nil)
 	stopButton := widget.NewButtonWithIcon("", theme.MediaStopIcon(), nil)
 	nextButton := widget.NewButtonWithIcon("", theme.MediaSkipNextIcon(), nil)
 	settingsButton := widget.NewButtonWithIcon("", theme.SettingsIcon(), nil)
-	buttons := container.NewHBox(layout.NewSpacer(), startButton, stopButton, nextButton, settingsButton, layout.NewSpacer())
+	buttons := container.NewHBox(layout.NewSpacer(), playButton, stopButton, nextButton, settingsButton, layout.NewSpacer())
 
 	onPlay := func() {
 		if myPomodoro.Running {
-			startButton.Icon = theme.MediaPlayIcon()
-			startButton.Refresh()
+			playButton.Icon = theme.MediaPlayIcon()
+			playButton.Refresh()
 
 			myPomodoro.Pause()
 		} else {
-			startButton.Icon = theme.MediaPauseIcon()
-			startButton.Refresh()
+			playButton.Icon = theme.MediaPauseIcon()
+			playButton.Refresh()
 
 			myPomodoro.Start()
 		}
@@ -58,16 +58,16 @@ func MakeClassicLayout(app fyne.App, myPomodoro *pomodoro.Pomodoro) fyne.CanvasO
 
 		timer.Text = formatDuration(myPomodoro.RemainingTime)
 		timer.Refresh()
-		startButton.Icon = theme.MediaPlayIcon()
-		startButton.Refresh()
+		playButton.Icon = theme.MediaPlayIcon()
+		playButton.Refresh()
 	}
 	onNext := func() {
 		myPomodoro.Next()
 
 		timer.Text = formatDuration(myPomodoro.RemainingTime)
 		timer.Refresh()
-		startButton.Icon = theme.MediaPlayIcon()
-		startButton.Refresh()
+		playButton.Icon = theme.MediaPlayIcon()
+		playButton.Refresh()
 	}
 	onSettings := func() {
 		win := app.NewWindow("Settings")
@@ -89,7 +89,7 @@ func MakeClassicLayout(app fyne.App, myPomodoro *pomodoro.Pomodoro) fyne.CanvasO
 		win.Show()
 	}
 
-	startButton.OnTapped = onPlay
+	playButton.OnTapped = onPlay
 	timerButton.OnTapped = onPlay
 	stopButton.OnTapped = onStop
 	nextButton.OnTapped = onNext
@@ -102,8 +102,8 @@ func MakeClassicLayout(app fyne.App, myPomodoro *pomodoro.Pomodoro) fyne.CanvasO
 	myPomodoro.OnEnd = func(kind pomodoro.Kind) {
 		timer.Text = formatDuration(myPomodoro.RemainingTime)
 		timer.Refresh()
-		startButton.Icon = theme.MediaPlayIcon()
-		startButton.Refresh()
+		playButton.Icon = theme.MediaPlayIcon()
+		playButton.Refresh()
 
 		notifyPomodoroDone(kind)
 	}
