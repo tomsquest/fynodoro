b4a8a74632573ab60412a2ea4a588ab044b865f0
Thomas Queste <tom@tomsquest.com>
2024-09-26 06:39:41 +0200

    feat: add about dialog in tray (#28)
    * feat: add about dialog in tray
    
    * feat: set fixed size of main windows (disable resizing)

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
 	"github.com/tomsquest/fynodoro/ui"
 )
 
+// Variables set by goreleaser
+// We need to put them in a Struct to be able to access them in the UI
+var (
+	version    string
+	commit     string
+	commitDate string
+)
+
 func main() {
 	myApp := app.NewWithID("com.tomsquest.fynodoro")
 	myApp.Settings().SetTheme(&ui.Theme{})
 	myApp.SetIcon(ui.AssetIconPng)
 
-	ui.Display(myApp)
+	ui.Display(myApp, ui.BuildInfo{version, commit, commitDate})
 }
